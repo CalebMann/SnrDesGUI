7d3a22507edc015f94c46c063ae810039e8bdfcc
kaylamf25 <46701855+kaylamf25@users.noreply.github.com>
2020-02-22 17:09:53 -0600

    Connection with Twilio set up

--- src/sample/GUI.java
+++ src/sample/GUI.java
@@ -2,16 +2,14 @@ package sample;
 
 import  javax.swing.*;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.*;
 import java.util.ArrayList;
 
 public class GUI extends JFrame
 {
     private static DatagramSocket socket;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ShowTempFahrOrCel frame = new ShowTempFahrOrCel();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
@@ -19,41 +17,36 @@ public class GUI extends JFrame
 
         try // create DatagramSocket for sending and receiving packets
         {
-            socket = new DatagramSocket(5000);
+            socket = new DatagramSocket();
         } catch (SocketException socketException) {
             socketException.printStackTrace();
             System.exit(1);
         }
 
-    }
-
-    public void waitForPackets() {
-        while (true){
-            try {
-                byte[] data = new byte[1000]; // set up packet
-                DatagramPacket receivePacket =
-                        new DatagramPacket(data, data.length);
-                socket.receive(receivePacket); // wait to receive packet
-                String packetData = new String(receivePacket.getData(),
-                        0, receivePacket.getLength()).toLowerCase();
-
-
-                receivePacket.setData(("Some Words That are Temporary").getBytes());
-
-                sendPacketToMaven(receivePacket); // send packet to client
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
+        try
+        {
+            String test = "-100";
+            byte[] bytes = test.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(
+                    bytes, bytes.length,
+                    InetAddress.getLocalHost(), 5000);
+            sendPacketToMaven(sendPacket);
         }
+        catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+
     }
 
-    private void sendPacketToMaven(DatagramPacket receivePacket)
+
+    private static void sendPacketToMaven(DatagramPacket receivePacket)
             throws IOException {
         DatagramPacket sendPacket = new DatagramPacket(
                 receivePacket.getData(), receivePacket.getLength(),
                 receivePacket.getAddress(), receivePacket.getPort());
         socket.send(sendPacket); // send packet to client
-        System.out.println("Sent: " + receivePacket.getData().toString());
+        System.out.println("Sent: " + new String(receivePacket.getData()));
     }
 
 }
