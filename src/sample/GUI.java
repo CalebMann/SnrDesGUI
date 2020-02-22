06aa74e89093783db1d4f1f1dd84e2bf611aa0ea
Caleb-Mann <calmann@uiowa.edu>
2020-02-22 16:31:07 -0600

    added basic networking stuff

--- src/sample/GUI.java
+++ src/sample/GUI.java
@@ -1,13 +1,59 @@
 package sample;
 
 import  javax.swing.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
 
-public class GUI
+public class GUI extends JFrame
 {
-    public static void main(String[] args) {
+    private static DatagramSocket socket;
+
+    public static void main(String[] args){
         ShowTempFahrOrCel frame = new ShowTempFahrOrCel();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
         frame.setVisible(true);
+
+        try // create DatagramSocket for sending and receiving packets
+        {
+            socket = new DatagramSocket(5000);
+        } catch (SocketException socketException) {
+            socketException.printStackTrace();
+            System.exit(1);
+        }
+
     }
+
+    public void waitForPackets() {
+        while (true){
+            try {
+                byte[] data = new byte[1000]; // set up packet
+                DatagramPacket receivePacket =
+                        new DatagramPacket(data, data.length);
+                socket.receive(receivePacket); // wait to receive packet
+                String packetData = new String(receivePacket.getData(),
+                        0, receivePacket.getLength()).toLowerCase();
+
+
+                receivePacket.setData(("Some Words That are Temporary").getBytes());
+
+                sendPacketToMaven(receivePacket); // send packet to client
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    private void sendPacketToMaven(DatagramPacket receivePacket)
+            throws IOException {
+        DatagramPacket sendPacket = new DatagramPacket(
+                receivePacket.getData(), receivePacket.getLength(),
+                receivePacket.getAddress(), receivePacket.getPort());
+        socket.send(sendPacket); // send packet to client
+        System.out.println("Sent: " + receivePacket.getData().toString());
+    }
+
 }
