a668dd4e9454b59b841b7cb3585eceee0c8ccb5d
kaylamf25 <46701855+kaylamf25@users.noreply.github.com>
2020-02-24 10:57:37 -0600

    switch person

--- src/sample/Graph.java
+++ src/sample/Graph.java
@@ -2,31 +2,21 @@ package sample;
 
 import javax.swing.*;
 
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 
 public class Graph extends JPanel
 {
-//    NumberAxis xAxis = new NumberAxis(0, 10, 1);
-//    NumberAxis yAxis = new NumberAxis(-100, 500, 100);
-//    ScatterChart<Number,Number> sc = new
-//            ScatterChart<Number,Number>(xAxis,yAxis);
-//
-//    public Graph()
-//    {
-//        xAxis.setLabel("Age (years)");
-//        yAxis.setLabel("Returns to date");
-//        sc.setTitle("Investment Overview");
-//
-//        XYChart.Series series1 = new XYChart.Series();
-//        series1.setName("Equities");
-//        series1.getData().add(new XYChart.Data(4.2, 193.2));
-//
-//        sc.getData().addAll(series1);
-//        //Scene scene  = new Scene(sc, 500, 400);
-//        //stage.setScene(scene);
-//        //stage.show();
-//    }
+    final NumberAxis xAxis;
+    final NumberAxis yAxis;
+    public Graph()
+    {
+        xAxis = new NumberAxis();
+        yAxis = new NumberAxis();
+        xAxis.setLabel("Seconds ago");
+
+    }
 
 }
