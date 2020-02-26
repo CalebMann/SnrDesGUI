f9e1b57f720ff44921caa6da8bff69978574c699
Caleb-Mann <calmann@uiowa.edu>
2020-02-26 11:38:38 -0600

    data manamgement

--- src/sample/GraphThread.java
+++ src/sample/GraphThread.java
@@ -16,25 +16,29 @@ public class GraphThread implements Runnable
     @Override
     public void run()
     {
-        int currentPointer = GUI.SharedData.dataPointer;
-        GUI.SharedData.incDP();
+        try{
+            int currentPointer = GUI.SharedData.dataPointer;
+            GUI.SharedData.incDP();
+            if(series != null){
+                series.getData().clear();
+            }
+            System.out.println("Graphing1: "+GUI.SharedData.data[currentPointer]);
 
-        series.getData().clear();
-        System.out.println("Graphing1: "+GUI.SharedData.data[currentPointer]);
-
-        for(int i=0; i<299; i++){
-            System.out.println("Graphing2: "+GUI.SharedData.data[currentPointer]);
-
-            if(GUI.SharedData.data[currentPointer] != null){
-                System.out.println("Graphing3: "+GUI.SharedData.data[currentPointer]);
-                series.getData().add(new XYChart.Data(-1*i,GUI.SharedData.data[currentPointer]));
-                if(currentPointer==0){
-                    currentPointer=299;
-                }else{
-                    currentPointer--;
+            for(int i=0; i<299; i++){
+                if(GUI.SharedData.data[currentPointer] != null){
+                    System.out.println("Graphing3: "+GUI.SharedData.data[currentPointer]);
+                    series.getData().add(new XYChart.Data(-1*i,GUI.SharedData.data[currentPointer]));
+                    if(currentPointer==0){
+                        currentPointer=299;
+                    }else{
+                        currentPointer--;
+                    }
                 }
             }
+        }catch (Exception e){
+            System.out.println(e);
         }
+
 
 
 
